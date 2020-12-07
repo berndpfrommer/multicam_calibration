1a9e729c17da6e16db83fa4449b13f62e467f408
Bernd Pfrommer <bernd.pfrommer@gmail.com>
2020-12-07 12:10:47 -0500

    install catkin_simple

--- .github/scripts/build_xenial.bash
+++ .github/scripts/build_xenial.bash
@@ -1,10 +1,19 @@
 #!/bin/bash
 ls -la
 src_dir=pwd
+ls -latr /opt/ros/
+# set up ROS
+source /opt/ros/xenial/setup.bash
 
+# make workspace
 mkdir -p ../catkin_ws/src
 pushd ../catkin_ws
-catkin config -DCMAKE_BUILD_TYPE=RelWithDebInfo
+
+# bring necessary workspaces into the picture
+git clone https://github.com/catkin/catkin_simple.git src/catkin_simple
 ln -s $src_dir src/multicam_calibration
+
+catkin config -DCMAKE_BUILD_TYPE=RelWithDebInfo
+
 catkin build
 
