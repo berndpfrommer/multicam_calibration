97c6da7c53077644392cb38f6d24b0e4331c6bab
Bernd Pfrommer <bernd.pfrommer@gmail.com>
2020-12-07 11:49:04 -0500

    try compiling

--- .github/scripts/build_xenial.bash
+++ .github/scripts/build_xenial.bash
@@ -1,3 +1,10 @@
 #!/bin/bash
 ls -la
-pwd
+src_dir=pwd
+
+mkdir -p ../catkin_ws/src
+pushd ../catkin_ws
+catkin config -DCMAKE_BUILD_TYPE=RelWithDebInfo
+ln -s $src_dir src/multicam_calibration
+catkin build
+
