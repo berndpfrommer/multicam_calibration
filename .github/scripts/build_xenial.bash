ee0440653e82e0360f31a1872558547feb647c96
Bernd Pfrommer <bernd.pfrommer@gmail.com>
2020-12-07 12:14:41 -0500

    source kinetic, not xenial

--- .github/scripts/build_xenial.bash
+++ .github/scripts/build_xenial.bash
@@ -3,14 +3,16 @@ ls -la
 src_dir=pwd
 ls -latr /opt/ros/
 # set up ROS
-source /opt/ros/xenial/setup.bash
+source /opt/ros/kinetic/setup.bash
 
 # make workspace
 mkdir -p ../catkin_ws/src
 pushd ../catkin_ws
 
-# bring necessary workspaces into the picture
+# clone and link all packages
+
 git clone https://github.com/catkin/catkin_simple.git src/catkin_simple
+git clone --recursive https://github.com/versatran01/apriltag.git src/apriltag
 ln -s $src_dir src/multicam_calibration
 
 catkin config -DCMAKE_BUILD_TYPE=RelWithDebInfo
